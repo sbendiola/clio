c2fdeb764b1f0f08fb54cbbe7c3267ac147d232e
Steve Bendiola <sbendiola@steve-bendiolas-computer.local>
2010-03-15 22:13:30 -0500

    more tests

--- src/main/scala/org/github/clio/Clio.scala
+++ src/main/scala/org/github/clio/Clio.scala
@@ -1,17 +1,33 @@
 package org.github.clio
 
-case class Opt(name: String, flag: Boolean = false, parent: Option[Opt] = None) {
-  def as[T](implicit converter: conversions.Converter[T]) = OptValue[T](this)(converter)
-  def apply(name: String) = {
-    Opt(name, parent = Some(this))
-  }
-}
-
-case class OptValue[T](o: Opt)(implicit converter: conversions.Converter[T])  
-
-class Clio(args: Array[String]) {
-  def o(name: String) = Opt(name)
+class Clio(val args: Array[String]) {
+  var values = List[Value]()
+  def o = Opt
+    
   def f(name: String) = Opt(name, flag = true)
+  
+  sealed trait Value {
+    println(values)
+  }
+  
+  case class OptValue[T](o: Opt, multiValue: Boolean = false)(implicit converter: conversions.Converter[T]) extends Value {
+    values = values.filterNot(_ == o)
+    values ::= this
+  }
+  
+  case class Opt(name: String, longName: String = "", description: String = "", required: Boolean = false, flag: Boolean = false, parent: Option[Opt] = None) extends Value {
+    values ::= this
+    def as[T](implicit converter: conversions.Converter[T]) = 
+      OptValue[T](this)(converter)
+
+    def asList = OptValue[String](this, multiValue = true)(conversions.StringConverter)
+
+    def asList[T](implicit converter: conversions.Converter[T]) = 
+        OptValue[T](this, multiValue = true)(converter)
+
+    def apply(name: String, longName: String = "", required: Boolean = false, description: String = "", flag: Boolean = false) = 
+      new Opt(name = name, longName = longName, description = description, required = required, flag = flag, parent = Some(this))  
+  }  
 }
 
 object conversions {  
@@ -21,5 +37,17 @@ object conversions {
   
   implicit object IntConverter extends Converter[Int] {
     def apply(text: String) = text.toInt
+  }
+  
+  implicit object FloatConverter extends Converter[Float] {
+    def apply(text: String) = text.toFloat
+  }
+
+  implicit object ShortConverter extends Converter[Short] {
+    def apply(text: String) = text.toShort
+  }  
+  
+  implicit object StringConverter extends Converter[String] {
+    def apply(text: String) = text
   }
 }
--- src/test/scala/org/github/clio/ClioSpec.scala
+++ src/test/scala/org/github/clio/ClioSpec.scala
@@ -4,7 +4,8 @@ import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
 
 class ClioSpec extends FlatSpec with ShouldMatchers {
-  it should "pop values in last-in-first-out order" in {
+  
+  it should "support having the key and value in the same arg" in {
     pending
     val args = Array("--block-size=10")
     val clio = new Clio(args) {
@@ -13,18 +14,134 @@ class ClioSpec extends FlatSpec with ShouldMatchers {
     clio.blockSize should be (10)    
   }
   
-  it should "throw NoSuchElementException if an empty stack is popped" in {
+  it should "should set a property when the value follows the designator" in {
     pending
     val args = Array( "-buildfile", "mybuild.xml", "-Dproperty=value", "-Dproperty1=value1", "-projecthelp")
     val clio = new Clio(args) {
-      val buildFile = o("--buildfile")
       val property = o("-D")("property")
-      val property1 = o("-D")("property1")
-      val projectHelp = f("-projecthelp")
-    }
-    clio.buildFile should be ("mybuild.xml")
+    }      
     clio.property should be ("value")
-    clio.property1 should be ("value1")
     //clio.projectHelp should be (true)
   }
+
+  it should "should support key with value" in {
+    pending
+    val args = Array("-ddebug")
+    val clio = new Clio(args) {
+      val debugFlag = o("-d")
+    }      
+    clio.debugFlag should be ("debug")
+  }
+
+  it should "should set flags" in {
+    pending
+    val args = Array("-projecthelp")
+    val clio = new Clio(args) {
+      val projectHelp = f("-projecthelp")
+    }      
+    clio.projectHelp should be ("true")
+  }
+  
+  it should "allow clustering flags" in {
+    pending
+    val args = Array("-abc")
+    val clio = new Clio(args) {
+      val a = f("-a")
+      val b = f("-b")
+      val c = f("-c")
+    }
+    clio.a should be ("true")
+    clio.b should be ("true")
+    clio.c should be ("true")
+  }
+
+  it should "allow clustering flags in any order" in {
+    pending
+    val args = Array("-cab")
+    val clio = new Clio(args) {
+      val a = f("-a")
+      val b = f("-b")
+      val c = f("-c")
+    }
+    clio.a should be ("true")
+    clio.b should be ("true")
+    clio.c should be ("true")
+  }
+  
+  it should "display help" in {
+    pending
+    val args = Array("-h")
+    
+    class HelpException extends Exception
+    intercept[HelpException] {
+      val clio = new Clio(args) {
+        val a = f("-a")
+        val b = f("-b")
+        val c = f("-c")
+      }  
+    }
+  }
+
+  it should "display help on long flag" in {
+    pending
+    val args = Array("--help")
+    
+    class HelpException extends Exception
+    intercept[HelpException] {
+      val clio = new Clio(args) {
+        val a = f("-a")
+        val b = f("-b")
+        val c = f("-c")
+      }  
+    }
+  }
+
+  it should "display help on ?" in {
+    pending
+    val args = Array("?")
+    
+    class HelpException extends Exception
+    intercept[HelpException] {
+      val clio = new Clio(args) {
+        val a = f("-a")
+        val b = f("-b")
+        val c = f("-c")
+      }  
+    }
+  }
+
+
+  it should "display help on help" in {
+    pending
+    val args = Array("help")
+    
+    class HelpException extends Exception
+    intercept[HelpException] {
+      val clio = new Clio(args) {
+        val a = f("-a")
+        val b = f("-b")
+        val c = f("-c")
+      }  
+    }
+  }
+
+  
+  it should "should support long names" in {
+    pending
+    val args = Array( "--buildfile", "mybuild.xml")
+    val clio = new Clio(args) {
+      val buildFile = o("-b", "--buildfile")
+    }      
+    clio.buildFile should be ("mybuild.xml")
+  }
+  
+  it should "should support multi args names" in {
+    pending
+    val args = Array("-f", "file1.xml", "file2.xml", "file3.xml")
+    val clio = new Clio(args) {
+      val files = o("-f").asList
+    }      
+    clio.files should be (List("file1.xml", "file2.xml", "file3.xml"))
+  }
+
 }
